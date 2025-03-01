fcce233071da90a4c82109910496e734b6689822
roberta34 <buzenchiroberta@gmail.com>
2025-03-01 23:04:36 +0200

    Am pus intrebarile intr-un nested array in questions.js si m am ocupat un pic de design.

---
+++ Quiz Game with Timer/javascript/questions.js
@@ -0,0 +1,170 @@
+//here I will write the JS code for the questions
+//the questions array
+//it will a nested array(similar to a words vector in C)
+let questions = [
+    {
+        number: 1,
+        question: "What is the chemical symbol for the element potassium?",
+        answer: "K",
+        options:[
+            "a) P",
+            "b) Po",
+            "c) K",
+            "d) Ps"
+        ]
+    },
+    {
+        number: 2,
+        question: "Who developed the theory of general relativity?",
+        answer: "Albert Einstein",
+        options:[
+            "a) Albert Einstein",
+            "b) Isaac Newton",
+            "c) Nikola Tesla",
+            "d) Robert Oppenheimer"
+        ]
+    },
+    {
+        number: 3,
+        question: "What is the formula for gravity?",
+        answer: "m*g",
+        options:[
+            "a) F*d",
+            "b) m*g",
+            "c) m*v",
+            "d) m*a"
+        ]
+    },
+    {
+        number: 4,
+        question: "What is the powerhouse of the cell?",
+        answer: "Mitochondrion",
+        options:[
+            "a) Heart",
+            "b) Ribosome",
+            "c) Nucleus",
+            "d) Mitochondrion"
+        ]
+    },
+    {
+        number: 5,
+        question: "Which programming language is known for its use in data science?",
+        answer: "Python",
+        options:[
+            "a) C++",
+            "b) Python",
+            "c) Java",
+            "d) HTML"
+        ]
+    },
+    {
+        number: 6,
+        question: " What does RAM stand for in computing?",
+        answer: "Random Access Memory",
+        options:[
+            "a) Random Access Memory",
+            "b) Randomized Access Memory",
+            "c) Read Access Memory",
+            "d) Rapid Access Memory"
+        ]
+    },
+    {
+        number: 7,
+        question: "Which of these is an open-source operating system?",
+        answer: "Linux",
+        options:[
+            "a) Windows 11",
+            "b) Windows 7",
+            "c) Microsoft",
+            "d) Linux"
+        ]
+    },
+    {
+        number: 8,
+        question: "What does CPU stand for in computing?",
+        answer: "Central Processing Unit",
+        options:[
+            "a) Central Professor Unit",
+            "b) Central Process Unit",
+            "c) Central Processing Unit",
+            "d) Central Program Unit"
+        ]
+    },
+    {
+        number: 9,
+        question: "Who wrote the play <i>Romeo and Juliet</i>?",
+        answer: "William Shakespeare",
+        options:[
+            "a) J.D. Salinger",
+            "b) Atticus Finch",
+            "c) William Shakespeare",
+            "d) Charles Dickens"
+        ]
+    },
+    {
+        number: 10,
+        question: "What is the title of the last Harry Potter book?",
+        answer: "Harry Potter and the Deathly Hollows",
+        options:[
+            "a) Harry Potter and the Philosopher's Stone",
+            "b) Harry Potter and the Deathly Hollows",
+            "c) Harry Potter and the Goblet of Fire",
+            "d) Harry Potter and the Prisoner of Azkaban"
+        ]
+    },
+    {
+        number: 11,
+        question: "Who wrote <i>The Odyssey</i>?",
+        answer: "Homer",
+        options:[
+            "a) Homer",
+            "b) Aristophanes",
+            "c) Platon",
+            "d) Sophocles"
+        ]
+    },
+    {
+        number: 12,
+        question: "Which book series features the character <i>Frodo Baggins</i>?",
+        answer: "The Lord of the Rings",
+        options:[
+            "a) The Chronicles of Narnia",
+            "b) A Song of Ice and Fire",
+            "c) Harry Potter",
+            "d) The Lord of the Rings"
+        ]
+    },
+    {
+        number: 13,
+        question: "Who was the first President of the United States?",
+        answer: "George Washington",
+        options:[
+            "a) George Washington",
+            "b) Thomas Jefferson",
+            "c) Abraham Lincoln",
+            "d) John Adams"
+        ]
+    },
+    {
+        number: 14,
+        question: "Which year did World War II end?",
+        answer: "1945",
+        options:[
+            "a) 1918",
+            "b) 1945",
+            "c) 1939",
+            "d) 1914"
+        ]
+    },
+    {
+        number: 15,
+        question: "In which year did the Titanic sink?",
+        answer: "1912",
+        options:[
+            "a) 1960",
+            "b) 1812",
+            "c) 1912",
+            "d) 2005"
+        ]
+    },
+]
